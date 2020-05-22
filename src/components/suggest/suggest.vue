67f80ae6eb57466921a750444a7244d7ce4292c8
nihaowodou <215966931@qq.com>
2020-05-22 23:11:38 +0800

    5-22

---
+++ src/components/suggest/suggest.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="suggest">
+    <ul class="suggest-list">
+      <li class="suggest-item" v-for="(item,index) in result" :key="index">
+        <div class="icon">
+          <i :class="getIconCls(item)"></i>
+        </div>
+        <div class="name">
+          <p class="text" v-html="getDisplayName(item)"></p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { search } from 'api/search'
+import { createSong, isValidMusic, processSongsUrl } from 'common/js/song'
+import { ERR_OK } from 'api/config'
+
+const TYPE_SINGER = 'singer'
+const perpage = 20 // 每页数据条数
+
+export default {
+  props: {
+    showSinger: {
+      type: Boolean,
+      default: true
+    },
+    query: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      page: 1,
+      // pullup: true,
+      // beforeScroll: true,
+      // hasMore: false,
+      result: []
+    }
+  },
+  watch: {
+    query () {
+      this.search()
+    }
+  },
+  methods: {
+    search () {
+      search(this.query, this.page, this.showSinger, perpage).then((res) => {
+        if (res.code === ERR_OK) {
+          this._genResult(res.data).then((result) => {
+            this.result = result
+          })
+        }
+      })
+    },
+    getDisplayName (item) {
+      if (item.type === TYPE_SINGER) {
+        return item.singername
+      } else {
+        return `${item.name}-${item.singer}`
+      }
+    },
+    getIconCls (item) {
+      if (item.type === TYPE_SINGER) {
+        return 'icon-mine'
+      } else {
+        return 'icon-music'
+      }
+    },
+    _genResult (data) {
+      let ret = []
+      // 表示有歌手的数据
+      if (data.zhida && data.zhida.singerid) {
+        ret.push({ ...data.zhida, ...{ type: TYPE_SINGER } })
+      }
+      // 处理 song 数据
+      return processSongsUrl(this._normalizeSongs(data.song.list)).then((songs) => {
+        ret = ret.concat(songs)
+        return ret
+      })
+    },
+    _normalizeSongs (list) {
+      let ret = []
+      list.forEach((musicData) => {
+        if (isValidMusic(musicData)) {
+          ret.push(createSong(musicData))
+        }
+      })
+      return ret
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+
+  .suggest
+    height: 100%
+    overflow: hidden
+    .suggest-list
+      padding: 0 30px
+      .suggest-item
+        display: flex
+        align-items: center
+        padding-bottom: 20px
+      .icon
+        flex: 0 0 30px
+        width: 30px
+        [class^="icon-"]
+          font-size: 14px
+          color: $color-text-d
+      .name
+        flex: 1
+        font-size: $font-size-medium
+        color: $color-text-d
+        overflow: hidden
+        .text
+          no-wrap()
+    .no-result-wrapper
+      position: absolute
+      width: 100%
+      top: 50%
+      transform: translateY(-50%)
+</style>
